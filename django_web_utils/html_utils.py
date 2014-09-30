96e24680894b380d1d0cd4d1e16866095c1125b7
Stéphane Diemer <stephane.diemer@ubicast.eu>
2014-09-30 10:39:12 +0200

    Added get_short_text function in html utils.

--- django_web_utils/html_utils.py
+++ django_web_utils/html_utils.py
@@ -6,13 +6,16 @@ HTML utility functions
 import traceback
 import re
 import htmlentitydefs
+from HTMLParser import HTMLParser
+import logging
+logger = logging.getLogger('djwutils.html_utils')
 # Django
 from django.template import defaultfilters
 from django.utils.safestring import mark_safe
 
 
 # unescape function
-#-----------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 def unescape(text):
     text = defaultfilters.striptags(text)
     def fixup(m):
@@ -36,7 +39,7 @@ def unescape(text):
     return re.sub("&#?\w+;", fixup, text)
 
 # get_meta_tag_text function
-#-----------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 def get_meta_tag_text(text):
     result = defaultfilters.striptags(text)
     result = unescape(result)
@@ -44,7 +47,7 @@ def get_meta_tag_text(text):
     return result
 
 # get_html_traceback function
-#-----------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 def get_html_traceback(tb=None):
     if not tb:
         tb = traceback.format_exc()
@@ -57,4 +60,84 @@ def get_html_traceback(tb=None):
         else:
             lines.append(line)
     return mark_safe(u'\n<br/>'.join(lines))
+
+# get_short_text function
+#------------------------------------------------------------------------------
+class TextHTMLParser(HTMLParser):
+    def __init__(self, html_text, max_length=300):
+        HTMLParser.__init__(self)
+        self._short = ''
+        self._length = 0
+        self._stop = False
+        self._tags_to_end = list()
+        
+        self._max_length = max_length
+        self.feed(html_text)
+    
+    def handle_starttag(self, tag, attrs):
+        if not self._stop:
+            self._short += '<%s' %tag
+            for attr in attrs:
+                self._short += ' %s="%s"' %(attr[0], attr[1])
+            self._short += '>'
+            # add tag to list of tags to end
+            self._tags_to_end.insert(0, tag)
+
+    def handle_endtag(self, tag):
+        if not self._stop:
+            self._short += '</%s>' %tag
+            # remove tag to list of tags to end
+            if len(self._tags_to_end) != 0:
+                if self._tags_to_end[0] == tag:
+                    self._tags_to_end.pop(0)
+    
+    def handle_startendtag(self, tag, attrs):
+        if not self._stop:
+            self._short += '<%s' %tag
+            for attr in attrs:
+                self._short += ' %s="%s"' %(attr[0], attr[1])
+            self._short += '/>'
+    
+    def handle_data(self, data):
+        if not self._stop:
+            if self._length + len(data) > self._max_length:
+                self._stop = True
+                data_length = self._max_length - self._length
+                cut = data[:data_length]
+                splitted = cut.split(' ')
+                self._short += ' '.join(splitted[:(len(splitted) - 1)])
+                self._short += ' ...'
+                self._insert_tags_end()
+            else:
+                self._length += len(data)
+                self._short += data
+
+    def handle_charref(self, name):
+        if not self._stop:
+            self._short += '&#%s;' %name
+            self._length += 1
+    
+    def handle_entityref(self, name):
+        if not self._stop:
+            self._short += '&%s;' %name
+            self._length += 1
+    
+    def _insert_tags_end(self):
+        for tag in self._tags_to_end:
+            self._short += '</%s>' %tag
+    
+    def get_short(self):
+        return self._short
+
+def get_short_text(html_text, max_length=300, margin=100):
+    """
+    Function to get an html text which does not exceed a given number of chars.
+    """
+    if len(html_text) > max_length + margin:
+        try:
+            parser = TextHTMLParser(html_text, max_length)
+            return parser.get_short()
+        except Exception, e:
+            logger.error('Unable to create short html text. %s' %e)
+    return ''
 
