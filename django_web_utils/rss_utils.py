2a8dce72079d39d881cc2f7a44a7ace4ef15d508
Stéphane Diemer <stephane.diemer@ubicast.eu>
2015-05-19 11:19:37 +0200

    Changed timezone import is RSS utils (Django 1.8).

--- django_web_utils/rss_utils.py
+++ django_web_utils/rss_utils.py
@@ -4,20 +4,20 @@
 Rss utility functions
 '''
 # Django
-from django.utils.tzinfo import LocalTimezone
+from django.utils.timezone import LocalTimezone
 
 
 # get_locale_tz_datetime function
-#-----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 def get_locale_tz_datetime(dt):
     tz = LocalTimezone(dt)
     dt = dt.replace(tzinfo=tz)
     return dt
 
+
 # get_RFC_2822_format function
-#-----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 def get_RFC_2822_format(dt):
     if dt.tzinfo is None:
         dt = get_locale_tz_datetime(dt)
     return dt.strftime('%a, %d %b %Y %H:%M:%S %z')
-    
