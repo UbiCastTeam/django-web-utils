3337003aa6541a3474e05883dcf27b2f919ac879
Stéphane Diemer <stephane.diemer@ubicast.eu>
2015-06-01 11:00:44 +0200

    Fixed a bug in makemessages script.

--- django_web_utils/scripts/makemessages.sh
+++ django_web_utils/scripts/makemessages.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-if [[ $# > 0 ]]
-then
+if [[ $# > 0 ]]; then
     cd $1
 fi
 
 langs="en fr"
-if [[ $# > 1 ]]
-then
-    langs="$langs $2"
+if [[ $# > 1 ]]; then
+    if [[ $2 == "en" || $2 == "fr" ]]; then
+        langs="$2"
+    else
+        langs="$langs $2"
+    fi
 fi
 
 
 function maketranslations {
     for j in $langs; do
         echo -e "Generating messages: $j"
-        django-admin.py makemessages -l $j -e html,txt,xml
+        django-admin.py makemessages -l $j -e py,html,xml
         retcode=$?
         if [[ $retcode != 0 ]]; then
             echo -e "Command exited with code $retcode."
@@ -32,10 +34,10 @@ for i in *; do
     dir=${i}
     locale="$dir/locale"
     if [ -d $locale ]; then
-        echo -e "Generating translations for path: $dir"
+        echo -e "\033[94mGenerating translations for path: $dir\033[0m"
         cd $dir
         maketranslations
-        cd ..
+        cd -
         echo ""
         found=true
     fi
