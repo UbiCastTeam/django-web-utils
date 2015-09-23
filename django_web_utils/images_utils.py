4b1735d664ef4bd35bdf3690db33141c464ff6b9
Stéphane Diemer <stephane.diemer@ubicast.eu>
2015-09-23 10:34:20 +0200

    Changed images utils formatting.

--- django_web_utils/images_utils.py
+++ django_web_utils/images_utils.py
@@ -11,7 +11,7 @@ from files_utils import get_new_path
 
 
 # get_image_ratio function
-#-----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 def get_image_ratio(image_path):
     try:
         img = Image.open(image_path)
@@ -25,8 +25,9 @@ def get_image_ratio(image_path):
         else:
             return 0
 
+
 # get_image_info function
-#-----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 def get_image_info(path):
     file_size = 0
     file_extension = ''
@@ -45,8 +46,9 @@ def get_image_info(path):
         pass
     return dict(file_size=file_size, file_extension=file_extension, file_mtime=file_mtime, image_width=size[0], image_height=size[1])
 
+
 # rotate_image function
-#-----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 def rotate_image(path, clockwise=True, rename=True):
     img = Image.open(path)
     img.load()
@@ -58,12 +60,12 @@ def rotate_image(path, clockwise=True, rename=True):
     img.save(dest)
     return dest
 
+
 # encode_as_jpeg function
-#-----------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 def encode_as_jpeg(path, quality=85):
     img = Image.open(path)
     img.load()
     dest = get_new_path(path, new_extension='jpg')
     img.save(dest, 'JPEG', quality=quality)
     return dest
-
