7f35184dde042e461170851d66a8db96ac17e23e
Stéphane Diemer <stephane.diemer@ubicast.eu>
2015-11-04 18:11:08 +0100

    Added setdefaultencoding.

--- django_web_utils/scripts/get_sql_obj_dump.py
+++ django_web_utils/scripts/get_sql_obj_dump.py
@@ -9,6 +9,8 @@ import datetime
 from decimal import Decimal
 os.environ['LANG'] = 'en_US.UTF-8'
 os.environ['LC_ALL'] = 'en_US.UTF-8'
+reload(sys)  # reload sys to make setdefaultencoding available
+sys.setdefaultencoding('utf-8')
 
 SETTINGS_MODULE = 'settings'
 
