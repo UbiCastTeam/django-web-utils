3cf46f8f68bbcff5f40bc240660a0da0820bb6bf
Stéphane Diemer <stephane.diemer@ubicast.eu>
2016-06-29 11:55:37 +0200

    Added list fields, search fields and date hierarchy to register_module for Django admin.

--- django_web_utils/admin_utils.py
+++ django_web_utils/admin_utils.py
@@ -7,6 +7,20 @@ Django admin utility functions
 from django.contrib import admin
 from django.db import models as dj_models
 
+SEARCH_FIELDS = (
+    dj_models.CharField,
+    dj_models.TextField,
+    dj_models.SlugField,
+)
+DATE_FIELDS = (
+    dj_models.DateField,
+    dj_models.DateTimeField,
+)
+LIST_FIELDS = (
+    dj_models.BooleanField,
+    dj_models.NullBooleanField,
+)
+
 
 # register_module function
 # Automatic models registeration
@@ -20,16 +34,30 @@ def register_module(models_module):
             or not issubclass(model.__class__, dj_models.Model.__class__) \
             or not model.__module__.startswith(models_module.__name__):
             continue
-        
+
         fields = []
+        lfields = []
+        sfields = []
+        dfield = None
         for field in model._meta.fields:
             fields.append(field.name)
+            if isinstance(field, LIST_FIELDS):
+                lfields.append(field.name)
+            if isinstance(field, SEARCH_FIELDS):
+                sfields.append(field.name)
+            if not dfield and isinstance(field, DATE_FIELDS):
+                dfield = field.name
 
         class ModelOptions(admin.ModelAdmin):
             save_on_top = True
             list_display = []
             list_filter = []
+            search_fields = []
             ordering = ['-id']
+            date_hierarchy = None
         ModelOptions.list_display = fields
-        
+        ModelOptions.list_filter = lfields
+        ModelOptions.search_fields = sfields
+        ModelOptions.date_hierarchy = dfield
+
         admin.site.register(model, ModelOptions)
