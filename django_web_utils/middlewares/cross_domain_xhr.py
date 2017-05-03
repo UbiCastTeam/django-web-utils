f8dbab0b1fe73b2a117d141fc21602850ae9f266
Stéphane Diemer <stephane.diemer@ubicast.eu>
2017-05-03 11:18:35 +0200

    PEP8.

--- django_web_utils/middlewares/cross_domain_xhr.py
+++ django_web_utils/middlewares/cross_domain_xhr.py
@@ -2,14 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from django import http
+from django.conf import settings
 
-try:
-    from django.conf import settings
-    XS_SHARING_ALLOWED_ORIGINS = settings.XS_SHARING_ALLOWED_ORIGINS
-    XS_SHARING_ALLOWED_METHODS = settings.XS_SHARING_ALLOWED_METHODS
-except:
-    XS_SHARING_ALLOWED_ORIGINS = '*'
-    XS_SHARING_ALLOWED_METHODS = ['POST','GET','OPTIONS', 'PUT', 'DELETE']
+XS_SHARING_ALLOWED_ORIGINS = getattr(settings, 'XS_SHARING_ALLOWED_ORIGINS', '*')
+XS_SHARING_ALLOWED_METHODS = getattr(settings, 'XS_SHARING_ALLOWED_METHODS', ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE'])
 
 
 class XsSharing(object):
@@ -19,24 +15,19 @@ class XsSharing(object):
     Access-Control-Allow-Origin: http://foo.example
     Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE
     '''
-    
-    def process_request(self, request):
 
+    def process_request(self, request):
         if 'HTTP_ACCESS_CONTROL_REQUEST_METHOD' in request.META:
             response = http.HttpResponse()
             response['Access-Control-Allow-Origin'] = XS_SHARING_ALLOWED_ORIGINS
-            response['Access-Control-Allow-Methods'] = ','.join( XS_SHARING_ALLOWED_METHODS )
-            
+            response['Access-Control-Allow-Methods'] = ','.join(XS_SHARING_ALLOWED_METHODS)
             return response
-
         return None
 
     def process_response(self, request, response):
         # Avoid unnecessary work
         if response.has_header('Access-Control-Allow-Origin'):
             return response
-
         response['Access-Control-Allow-Origin'] = XS_SHARING_ALLOWED_ORIGINS
-        response['Access-Control-Allow-Methods'] = ','.join( XS_SHARING_ALLOWED_METHODS )
-
+        response['Access-Control-Allow-Methods'] = ','.join(XS_SHARING_ALLOWED_METHODS)
         return response
