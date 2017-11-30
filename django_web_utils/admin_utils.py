60e26a24d79cdeeaf7fd61851099ec166f68523e
Stéphane Schoorens <stephane.schoorens@ubicast.eu>
2017-11-30 16:30:21 +0100

    add csv export to django actions refs #23509

--- django_web_utils/admin_utils.py
+++ django_web_utils/admin_utils.py
@@ -3,9 +3,50 @@
 '''
 Django admin utility functions
 '''
+import csv
 # Django
 from django.contrib import admin
 from django.db import models as dj_models
+from django.http import HttpResponse
+
+
+def _export_as_csv_action(description="Export selected objects as CSV file", fields=None, exclude=None, header=True):
+    """
+    This function returns an export csv action
+    'fields' and 'exclude' work like in django ModelForm
+    'header' is whether or not to output the column names as the first row
+    """
+    def export_as_csv(modeladmin, request, queryset):
+        """
+        Generic csv export admin action.
+        based on http://djangosnippets.org/snippets/1697/
+        """
+        opts = modeladmin.model._meta
+        field_names = set([field.name for field in opts.fields])
+
+        if fields:
+            fieldset = set(fields)
+            field_names = field_names & fieldset
+
+        elif exclude:
+            excludeset = set(exclude)
+            field_names = field_names - excludeset
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=export.csv'
+
+        writer = csv.writer(response)
+
+        if header:
+            writer.writerow(list(field_names))
+        for obj in queryset:
+            writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = description
+    return export_as_csv
+
 
 SEARCH_FIELDS = (
     dj_models.CharField,
@@ -50,6 +91,7 @@ def register_module(models_module, options=dict()):
 
         class ModelOptions(admin.ModelAdmin):
             save_on_top = True
+            actions = []
             list_display = []
             list_filter = []
             search_fields = []
@@ -59,6 +101,7 @@ def register_module(models_module, options=dict()):
         ModelOptions.list_filter = lfields
         ModelOptions.search_fields = sfields
         ModelOptions.date_hierarchy = dfield
+        ModelOptions.actions = [_export_as_csv_action("CSV Export", fields=fields)]
 
         if options.get(attr_name):
             for key in options[attr_name].keys():
