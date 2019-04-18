4bb6e970094e3810ac1039217fff90bcdb2b95e1
Stéphane Diemer <stephane.diemer@ubicast.eu>
2019-04-18 11:55:44 +0200

    Use flake8 in pyflakescolor script.

--- django_web_utils/scripts/pyflakescolor.py
+++ django_web_utils/scripts/pyflakescolor.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Author: Stéphane Diemer stephane.diemer@ubicast.eu
+'''
+Script to get a colored output for the flake8 command.
+Author: Stéphane Diemer, stephane.diemer@ubicast.eu.
 
-from __future__ import print_function
+To use the last flake8 version on Ubuntu/Debian:
+sudo apt purge pyflakes3 python3-flake8 python3-mccabe python3-pycodestyle python3-pyflakes flake8 python-pyflakes pep8 pycodestyle pyflakes
+sudo apt install python3-pip
+sudo pip3  install --upgrade pip
+sudo pip3 install flake8
+
+Flake8 codes:
+https://pycodestyle.readthedocs.io/en/latest/intro.html#error-codes
+http://flake8.pycqa.org/en/latest/user/error-codes.html
+'''
 import os
-import sys
+import re
 import subprocess
+import sys
 
 
 class PyflakesColor(object):
+    __doc__
     RED = '\033[91m'
     GREEN = '\033[92m'
     YELLOW = '\033[93m'
@@ -16,89 +29,72 @@ class PyflakesColor(object):
     PURPLE = '\033[95m'
     TEAL = '\033[96m'
     DEFAULT = '\033[0m'
-    
+    IGNORED = 'E128,E501,E731,W504,W505'
+
     def __init__(self):
         object.__init__(self)
         self.returncode = 0
-    
+
     def run(self, *args):
         if len(args) > 1:
             paths_to_check = args[1:]
         else:
             paths_to_check = '.'
-        FNULL = open(os.devnull, 'w')
-        pyflakes_present = subprocess.call(['which', 'pyflakes'], stdout=FNULL, stderr=FNULL) == 0
-        if not pyflakes_present:
-            print('Please install pyflakes')
+        p = subprocess.run(['which', 'flake8'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        if p.returncode != 0:
+            print('Please install flake8 first', file=sys.stderr)
             sys.exit(1)
-        error, warning, info, other = self.pyflakes_check(paths_to_check)
-        print('Errors: %s, warnings: %s, information: %s, others: %s' % (error, warning, info, other))
+        error, warning, info = self.pyflakes_check(paths_to_check)
+        print('Errors: %s, warnings: %s, information: %s' % (error, warning, info))
         return self.returncode
-    
-    def pyflakes_check(self, paths, display_result_for_all_files=False):
-        error = warning = info = other = 0
+
+    def pyflakes_check(self, paths):
+        error = warning = info = 0
         if not isinstance(paths, list) and not isinstance(paths, tuple):
             paths = [paths]
         for path in paths:
             if not os.path.exists(path):
                 print('%sPath "%s" does not exists.%s' % (self.YELLOW, path, self.DEFAULT))
                 continue
-            if os.path.isfile(path):
-                base_path = os.path.dirname(path)
-                list_dir = [os.path.basename(path)]
-                display_result_for_all_files = True
-            elif os.path.isdir(path):
-                base_path = path
-                list_dir = os.listdir(path)
-                list_dir.sort()
+            # get flake8 command
+            cmd = ['flake8']
+            if self.IGNORED:
+                cmd.append('--ignore=%s' % self.IGNORED)
+            cmd.append(path)
+            p = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding='utf-8')
+            if p.returncode > self.returncode:
+                self.returncode = p.returncode
+            # parse flake8 result
+            lines = p.stdout.strip().split('\n') if p.stdout else list()
+            if not lines:
+                if os.path.isfile(path):
+                    print('%sFile: %s%s' % (self.PURPLE, path, self.DEFAULT))
+                    print('    File is OK\n')
             else:
-                print('%sPath "%s" is neither a file nor a directory.%s' % (self.YELLOW, path, self.DEFAULT))
-                continue
-            for picked_name in list_dir:
-                picked_path = os.path.join(base_path, picked_name)
-                if os.path.isfile(picked_path):
-                    if picked_name.endswith('.py'):
-                        text = 'file: %s \t path: %s' % (picked_name, picked_path)
-                        p = subprocess.Popen(['python3', '-m', 'pyflakes', picked_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                        out, err = p.communicate()
-                        # decode from bytes to unicode
-                        out = out.decode('utf-8') if out else ''
-                        err = err.decode('utf-8') if err else ''
-                        if p.returncode > self.returncode:
-                            self.returncode = p.returncode
-                        lines = out.split('\n') if out else list()
-                        if err:
-                            lines.extend(['        %s' % l if 'invalid syntax' not in l else l for l in err.split('\n')])
-                        if len(lines) == 0 or (len(lines) == 1 and lines[0].strip() == ''):
-                            if display_result_for_all_files:
-                                print('%s%s%s' % (self.BLUE, text, self.DEFAULT))
-                                print('    File is OK\n')
+                lines.sort()
+                last_path = None
+                for line in lines:
+                    if not line.strip():
+                        continue
+                    m = re.match(r'^(.*):(\d+):(\d+): (\w\d+) (.*)$', line)
+                    if not m:
+                        print(line)
+                    else:
+                        fpath, line_index, char_index, message_id, message_text = m.groups()
+                        if not last_path or last_path != fpath:
+                            last_path = fpath
+                            print('%s%s:%s' % (self.PURPLE, fpath, self.DEFAULT))
+                        if message_id in ('E265', 'E302', 'E402', 'F401', 'F403'):
+                            color = self.BLUE
+                            info += 1
+                        elif message_id in ('E741', 'F841', 'W293') or message_id[:2] in ('E3', 'E4', 'E5'):
+                            color = self.YELLOW
+                            warning += 1
                         else:
-                            print('%s%s%s' % (self.BLUE, text, self.DEFAULT))
-                            for line in lines:
-                                if not line.strip():
-                                    continue
-                                line = line.replace('%s:' % picked_path, 'line ')
-                                if 'unable to detect undefined names' in line:
-                                    color = self.GREEN
-                                    info += 1
-                                elif 'redefinition of function' in line or 'redefinition of unused' in line or 'is assigned to but never used' in line or 'imported but unused' in line:
-                                    color = self.YELLOW
-                                    warning += 1
-                                elif 'undefined name' in line or 'referenced before assignment' in line or 'invalid syntax' in line:
-                                    color = self.RED
-                                    error += 1
-                                else:
-                                    color = self.DEFAULT
-                                    other += 1
-                                print('    %s%s%s' % (color, line, self.DEFAULT))
-                elif os.path.isdir(picked_path):
-                    e, w, i, o = self.pyflakes_check(picked_path, display_result_for_all_files)
-                    error += e
-                    warning += w
-                    info += i
-                    other += o
-        return error, warning, info, other
+                            color = self.RED
+                            error += 1
+                        print('    %s%s:%s: %s %s%s' % (color, line_index, char_index, message_id, message_text, self.DEFAULT))
+        return error, warning, info
 
 
 if __name__ == '__main__':
