58463cd9cfecf44362281abdf7a4fcc972f9848e
Stéphane Diemer <stephane.diemer@ubicast.eu>
2022-02-08 09:13:24 +0100

    Handle antivirus scan errors | refs #25663 (#10)

--- django_web_utils/antivirus_utils.py
+++ django_web_utils/antivirus_utils.py
@@ -4,9 +4,19 @@
 Antivirus utility functions
 Requires:
  clamav-daemon
+
 The ClamAVDaemon code is coming from clammy repository:
 https://github.com/ranguli/clammy
 It was slightly modified to fit this lib needs.
+
+Settings:
+- ANTIVIRUS_SOCKET_PATH
+    The clamd socket path can be set in Django settings.
+    Default: '/var/run/clamav/clamd.ctl'
+- ANTIVIRUS_REPORTS_RECIPIENTS
+    The recipients for infected file upload report emails.
+    Can be a list of email addresses or a python module path to a callable returning the list.
+    Default: email adresses of settings.ADMINS
 '''
 from pathlib import Path
 import contextlib
@@ -27,20 +37,15 @@ from django_web_utils.module_utils import import_module_by_python_path
 
 logger = logging.getLogger('djwutils.antivirus_utils')
 
-# The clamd socket path can be set in Django settings.
-SOCKET_PATH = getattr(settings, 'ANTIVIRUS_SOCKET_PATH', '/var/run/clamav/clamd.ctl')
-
-# The recipients for infected file upload report emails.
-# Can be a list of email addresses or a python module path to a callable returning the list.
-REPORTS_RECIPIENTS = getattr(settings, 'ANTIVIRUS_REPORTS_RECIPIENTS', None)
-
 MIDDLEWARE_MODULE = 'django_web_utils.antivirus_utils.ReportInfectedFileUploadMiddleware'
 
-INFECTED_MESSAGE = _('Your file was rejected because it is infected.')
-DOES_NOT_EXIST_MESSAGE = _('Cannot scan file because it does not exist.')
-INVALID_PATH_MESSAGE = _('Cannot scan file because it is neither a file nor a directory.')
-COMMAND_ERROR_MESSAGE = _('Failed to run file scanner.')
 BAN_WARNING_MESSAGE = _('Your request was reported and if you send again infected files, you will be banned.')
+COMMAND_ERROR_MESSAGE = _('Failed to run antivirus scanner.')
+DOES_NOT_EXIST_MESSAGE = _('Cannot scan file because it does not exist.')
+INFECTED_MESSAGE = _('Your file was rejected because it is infected.')
+INVALID_FILE_MESSAGE = _('Cannot scan file because it is not a file.')
+INVALID_PATH_MESSAGE = _('Cannot scan file because it is neither a file nor a directory.')
+SCAN_FAILED_MESSAGE = _('Failed to scan your file with the antivirus.')
 
 
 class ClamdError(Exception):
@@ -191,7 +196,7 @@ class ClamAVDaemon:
         filename (string): filename or directory (MUST BE ABSOLUTE PATH !)
 
         return:
-          - (dict): {filename1: ('FOUND', 'virusname'), filename2: ('ERROR', 'reason')}
+          - (dict): {FOUND: 1, ERROR: 1, files: {filename1: ('FOUND', 'virusname'), filename2: ('ERROR', 'reason')}}
 
         May raise:
           - ClamdConnectionError: in case of communication problem
@@ -200,13 +205,15 @@ class ClamAVDaemon:
             self._init_socket()
             self._send_command(command, filename)
 
-            report = {'found': 0, 'files': {}}
+            report = {'files': {}}
             for result in self._recv_response_multiline().split('\n'):
                 if result:
                     filename, reason, status = self.parse_response(result)
                     report['files'][filename] = (status, reason)
-                    if status == 'FOUND':
-                        report['found'] += 1
+                    if status not in report:
+                        report[status] = 1
+                    else:
+                        report[status] += 1
 
             return report
 
@@ -222,7 +229,7 @@ class ClamAVDaemon:
           MUST be < StreamMaxLength in /etc/clamav/clamd.conf
 
         return:
-          - (dict): {filename1: ('virusname', 'status')}
+          - (dict): {FOUND: 1, files: {filename1: ('FOUND', 'virusname')}}
 
         May raise :
           - ClamdBufferTooLongError: if the buffer size exceeds clamd limits
@@ -247,7 +254,7 @@ class ClamAVDaemon:
                     raise ClamdBufferTooLongError(result)
 
                 filename, reason, status = self.parse_response(result)
-                report = {'found': (1 if status == 'FOUND' else 0), 'files': {filename: (status, reason)}}
+                report = {status: 1, 'files': {filename: (status, reason)}}
                 return report
         finally:
             self._close_socket()
@@ -354,11 +361,13 @@ def on_file_infected_error(request):
     log_msg += '\nThe file was uploaded on this URL: ' + ('https://' if request.is_secure() else 'http://') + request.get_host() + request.get_full_path()
     logger.warning(log_msg)
     # Get recipients
-    if isinstance(REPORTS_RECIPIENTS, str):
-        fct = import_module_by_python_path(REPORTS_RECIPIENTS)
+    # Recipients can be a list of email addresses or a python module path to a callable returning the list.
+    recipients_val = getattr(settings, 'ANTIVIRUS_REPORTS_RECIPIENTS', None)
+    if isinstance(recipients_val, str):
+        fct = import_module_by_python_path(recipients_val)
         recipients = fct()
-    elif isinstance(REPORTS_RECIPIENTS, (list, tuple, dict)):
-        recipients = REPORTS_RECIPIENTS
+    elif isinstance(recipients_val, (list, tuple, dict)):
+        recipients = recipients_val
     else:
         recipients = [adm[1] for adm in settings.ADMINS]
     # Send email if ricipients
@@ -408,6 +417,8 @@ def antivirus_path_validator(path, remove=True):
     '''
     Check given path (file or directory) and raise ValidationError if invalid or infected.
     The `path` argument must be a Path or a str.
+    Warning: The clamav unix user must be able to read the data to be able to scan it.
+    Use the `antivirus_file_validator` function to avoid this constraint.
     '''
     if isinstance(path, str):
         path = Path(path)
@@ -420,48 +431,76 @@ def antivirus_path_validator(path, remove=True):
         logger.warning('Cannot scan path "%s" because it is neither a file nor a directory.', path)
         raise ValidationError(INVALID_PATH_MESSAGE)
     try:
-        clamav = ClamAVDaemon(unix_socket=SOCKET_PATH)
+        sp = getattr(settings, 'ANTIVIRUS_SOCKET_PATH', None) or '/var/run/clamav/clamd.ctl'
+        clamav = ClamAVDaemon(unix_socket=sp)
         report = clamav.multi_scan(str(path))
+        logger.debug('Scanned with antivirus path "%s": %s', path, report)
     except Exception as e:
         logger.error('Scan failed for path "%s": %s', path, e)
         if remove:
             _remove_infected_file(path)
         raise ValidationError(COMMAND_ERROR_MESSAGE)
     else:
-        if report.get('found'):
+        if report.get('FOUND'):
             logger.warning('Path "%s" is infected%s:\n%s', path, (', it will be removed' if remove else ''), report['files'])
             if remove:
                 _remove_infected_file(path)
             if MIDDLEWARE_MODULE in settings.MIDDLEWARE:
                 raise FileInfectedError(INFECTED_MESSAGE)
             raise ValidationError(INFECTED_MESSAGE)
+        elif report.get('ERROR'):
+            logger.warning('Path "%s" cannot be scanned%s:\n%s', path, (', it will be removed' if remove else ''), report['files'])
+            raise ValidationError(SCAN_FAILED_MESSAGE)
         logger.debug('Path "%s" is not infected:\n%s', path, report['files'])
 
 
-def antivirus_file_validator(file, remove=True):
+def antivirus_stream_validator(stream, remove=True):
     '''
-    Check given uploaded file (for example in a model FileField) and raise ValidationError if invalid or infected.
-    The `file` argument must be a file object.
+    Check given file stream (for example in a model FileField) and raise ValidationError if invalid or infected.
+    The `stream` argument must be a file object.
     '''
-    # Ensure file pointer is at beginning of the file
-    file.seek(0)
+    # Ensure file stream pointer is at beginning of the file
+    stream.seek(0)
     try:
-        clamav = ClamAVDaemon(unix_socket=SOCKET_PATH)
-        report = clamav.instream(file)
+        sp = getattr(settings, 'ANTIVIRUS_SOCKET_PATH', None) or '/var/run/clamav/clamd.ctl'
+        clamav = ClamAVDaemon(unix_socket=sp)
+        report = clamav.instream(stream)
+        logger.debug('Scanned with antivirus file "%s": %s', stream.name, report)
     except Exception as e:
-        logger.error('Scan failed for uploaded file "%s": %s', file.name, e)
-        if remove and getattr(file, 'path', None):
-            _remove_infected_file(Path(file.path))
+        logger.error('Scan failed for stream "%s": %s', stream.name, e)
+        if remove and getattr(stream, 'path', None):
+            _remove_infected_file(Path(stream.path))
         raise ValidationError(COMMAND_ERROR_MESSAGE)
     else:
-        if report.get('found'):
-            logger.warning('Uploaded file "%s" is infected%s:\n%s', file.name, (', it will be removed' if remove else ''), report['files'])
-            if remove and getattr(file, 'path', None):
-                _remove_infected_file(Path(file.path))
+        if report.get('FOUND'):
+            logger.warning('Stream "%s" is infected%s:\n%s', stream.name, (', it will be removed' if remove else ''), report['files'])
+            if remove and getattr(stream, 'path', None):
+                _remove_infected_file(Path(stream.path))
             if MIDDLEWARE_MODULE in settings.MIDDLEWARE:
                 raise FileInfectedError(INFECTED_MESSAGE)
             raise ValidationError(INFECTED_MESSAGE)
-        logger.debug('Uploaded file "%s" is not infected:\n%s', file.name, report['files'])
+        elif report.get('ERROR'):
+            logger.warning('Stream "%s" cannot be scanned%s:\n%s', stream.name, (', it will be removed' if remove else ''), report['files'])
+            raise ValidationError(SCAN_FAILED_MESSAGE)
+        logger.debug('Stream "%s" is not infected:\n%s', stream.name, report['files'])
     finally:
-        # Return file pointer to beginning of the file again
-        file.seek(0)
+        # Return file stream pointer to beginning of the file again
+        stream.seek(0)
+
+
+def antivirus_file_validator(path, remove=True):
+    '''
+    Check given file path and raise ValidationError if invalid or infected.
+    The `path` argument must be a Path or a str.
+    This function allows to check paths inaccessible for the clamav user.
+    '''
+    if isinstance(path, str):
+        path = Path(path)
+    elif not isinstance(path, Path):
+        raise ValueError('Invalid argument type, a Path or a str is expected.')
+    if not path.is_file():
+        logger.warning('Cannot scan path "%s" because it is not a file.', path)
+        raise ValidationError(INVALID_FILE_MESSAGE)
+    with open(path, 'rb') as fo:
+        fo.path = path
+        antivirus_stream_validator(fo)
--- tests/testapp/forms.py
+++ tests/testapp/forms.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 from django import forms as dj_forms
 
-from django_web_utils.antivirus_utils import antivirus_file_validator
+from django_web_utils.antivirus_utils import antivirus_stream_validator
 
 
 class FileForm(dj_forms.Form):
-    file = dj_forms.FileField(label='File', required=True, validators=[antivirus_file_validator])
+    file = dj_forms.FileField(label='File', required=True, validators=[antivirus_stream_validator])
