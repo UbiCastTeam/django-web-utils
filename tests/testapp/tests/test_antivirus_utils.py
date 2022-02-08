13cda0db3eecd641a78a5de6ad706039cb97180e
Stéphane Diemer <stephane.diemer@ubicast.eu>
2022-02-08 09:09:38 +0100

    Handle antivirus scan errors | refs #25663

--- tests/testapp/tests/test_antivirus_utils.py
+++ tests/testapp/tests/test_antivirus_utils.py
@@ -2,6 +2,7 @@
 Antivirus utils tests.
 '''
 from io import BytesIO
+from os import chmod
 from pathlib import Path
 
 from django.conf import settings
@@ -10,7 +11,7 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import override_settings, TestCase
 from django.urls import reverse
 
-from django_web_utils.antivirus_utils import antivirus_path_validator, antivirus_file_validator, INFECTED_MESSAGE, DOES_NOT_EXIST_MESSAGE
+from django_web_utils import antivirus_utils as avu
 
 
 EICAR_TEST_CONTENT = 'X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'
@@ -35,33 +36,55 @@ class AntivirusUtilsTests(TestCase):
         with open(self.test_path, 'w') as fo:
             fo.write('Test content')
         # Should not raise any error
-        antivirus_path_validator(self.test_path)
+        avu.antivirus_path_validator(self.test_path)
 
     def test_infected_file(self):
         with open(self.test_path, 'w') as fo:
             # Write eicar test content
             fo.write(EICAR_TEST_CONTENT)
         # Should raise ValidationError
-        with self.assertRaisesMessage(ValidationError, str(INFECTED_MESSAGE)):
-            antivirus_path_validator(self.test_path)
+        with self.assertRaisesMessage(ValidationError, str(avu.INFECTED_MESSAGE)):
+            avu.antivirus_path_validator(self.test_path)
+
+    def test_non_existent_path(self):
+        # Should raise ValidationError
+        with self.assertRaisesMessage(ValidationError, str(avu.DOES_NOT_EXIST_MESSAGE)):
+            avu.antivirus_path_validator('/doesnotexist')
 
     def test_non_existent_file(self):
         # Should raise ValidationError
-        with self.assertRaisesMessage(ValidationError, str(DOES_NOT_EXIST_MESSAGE)):
-            antivirus_path_validator('/doesnotexist')
+        with self.assertRaisesMessage(ValidationError, str(avu.INVALID_FILE_MESSAGE)):
+            avu.antivirus_file_validator('/doesnotexist')
+
+    def test_restricted_file_denied(self):
+        with open(self.test_path, 'w') as fo:
+            # Write eicar test content
+            fo.write('Test content')
+        chmod(self.test_path, 0o400)
+        # Should raise ValidationError
+        with self.assertRaisesMessage(ValidationError, str(avu.SCAN_FAILED_MESSAGE)):
+            avu.antivirus_path_validator(self.test_path)
+
+    def test_restricted_file_ok(self):
+        with open(self.test_path, 'w') as fo:
+            # Write eicar test content
+            fo.write('Test content')
+        chmod(self.test_path, 0o400)
+        # Should not raise any error
+        avu.antivirus_file_validator(self.test_path)
 
     def test_clean_uploaded_file(self):
         upload_file = SimpleUploadedFile('test-clean.txt', b'Test content')
         upload_file.path = '/doesnotexist'
         # Should not raise any error
-        antivirus_file_validator(upload_file)
+        avu.antivirus_stream_validator(upload_file)
 
     def test_infected_uploaded_file(self):
         upload_file = SimpleUploadedFile('test-eicar.txt', EICAR_TEST_CONTENT.encode('utf-8'))
         upload_file.path = '/doesnotexist'
         # Should raise ValidationError
-        with self.assertRaisesMessage(ValidationError, str(INFECTED_MESSAGE)):
-            antivirus_file_validator(upload_file)
+        with self.assertRaisesMessage(ValidationError, str(avu.INFECTED_MESSAGE)):
+            avu.antivirus_stream_validator(upload_file)
 
     @override_settings(MIDDLEWARE=DEFAULT_MIDDLEWARES + (
         'django_web_utils.antivirus_utils.ReportInfectedFileUploadMiddleware',
