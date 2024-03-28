fe486eb990b17b96b53a78d6cb70a989a7826138
Stéphane Diemer <stephane.diemer@ubicast.eu>
2024-03-28 16:05:49 +0100

    Avoid dependency on the backend class in the magic login view | refs #39044

--- django_web_utils/magic_login/backend.py
+++ django_web_utils/magic_login/backend.py
@@ -6,19 +6,27 @@ from django.contrib.auth.backends import BaseBackend
 from django.contrib.auth.models import AbstractBaseUser
 from django.http import HttpRequest
 
+from django_web_utils.magic_login.tokens import check_token
+
 
 class MagicLoginBackend(BaseBackend):
+    """
+    Authentication backend for the magic login feature.
+    """
 
-    def authenticate(
-        self, request: HttpRequest, magic_login_user: Optional[AbstractBaseUser] = None
-    ) -> Optional[AbstractBaseUser]:
-        if not magic_login_user:
+    def authenticate(self, request: HttpRequest, magic_login_id: Optional[int]) -> Optional[AbstractBaseUser]:
+        if not magic_login_id:
             return
-        if magic_login_user.password:
+        user = self.get_user(user_id=magic_login_id)
+        if not user:
+            return
+        if not check_token(user, request.session.session_key, request.GET.get('t', '')):
+            return
+        if user.password:
             # Users using the magic link feature are not allowed to log in using a password
-            magic_login_user.password = ''
-            magic_login_user.save(update_fields=['password'])
-        return magic_login_user
+            user.password = ''
+            user.save(update_fields=['password'])
+        return user
 
     def get_user(self, user_id: int) -> Optional[AbstractBaseUser]:
         try:
